043cb24382aad06da24157a0cb6bbcbcc22b4798
Suhas Karanth <sudo.suhas@gmail.com>
2017-10-09 15:00:22 +0530

    build: Fix webpack umd build for UglifyJs options

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -31,24 +31,26 @@ module.exports = {
         ),
         new UglifyJSPlugin({
             sourceMap: false,
-            beautify: false,
-            mangle: {
-                toplevel: true,
-                keep_fnames: false
-            },
-            compressor: {
-                warnings: false,
-                conditionals: true,
-                unused: true,
-                comparisons: true,
-                sequences: true,
-                dead_code: true,
-                evaluate: true,
-                if_return: true,
-                join_vars: true,
-                negate_iife: false
-            },
-            comments: false
+            uglifyOptions: {
+                beautify: false,
+                mangle: {
+                    toplevel: true,
+                    keep_fnames: false
+                },
+                compressor: {
+                    warnings: false,
+                    conditionals: true,
+                    unused: true,
+                    comparisons: true,
+                    sequences: true,
+                    dead_code: true,
+                    evaluate: true,
+                    if_return: true,
+                    join_vars: true,
+                    negate_iife: false
+                },
+                comments: false
+            }
         })
     ]
 };
